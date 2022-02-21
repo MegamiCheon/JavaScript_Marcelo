404361b0a9d86091a73a78ddf0707058687a77d1
Megami.ay <laiz.detros@alunos.sc.senac.br>
2022-02-21 08:33:09 -0300

    a

--- marcelo_01/JS/JavaScript.js
+++ marcelo_01/JS/JavaScript.js
@@ -1,1 +1,9 @@
-alert("Mensagem fora do arquivo");
+alert("Hello World");
+
+var idade;
+var nome;
+
+prompt("Insira a sua idade");
+idade=16
+nome='Laiz'
+alert('olá ' +nome+ ' você tem ' +idade+ ' anos');
