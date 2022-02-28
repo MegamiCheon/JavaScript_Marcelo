9aabbb1fb557030f252a9b0bd477f8c1b1c962e0
Megami.ay <megami.ay@gmail.com>
2022-02-28 10:08:01 -0300

    ab

---
+++ Mar_02/JS/JS.js
@@ -0,0 +1,11 @@
+var nome
+var sobnom
+var idade
+var peso
+
+nome = window.prompt("Insira o nome: ");
+sobnom = window.prompt("Insira seu sobrenome: ");
+idade = window.prompt("Insira sua idade: ");
+peso = window.prompt("Insira seu pego em quilos: ");
+
+document.write("Seu nome é: " + nome + " " + sobnom + "<br>" + "Você tem: " + idade + "<br>" + "e você pesa: " + peso);
