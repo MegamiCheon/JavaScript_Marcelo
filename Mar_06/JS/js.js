cc7b140d27c2664fad645a06d6464c41480a891e
LAIZ BORDINHAO DETROS <laiz.detros@alunos.sc.senac.br>
2022-03-15 14:48:23 -0300

    abcdefg

--- Mar_06/JS/js.js
+++ Mar_06/JS/js.js
@@ -1,4 +1,4 @@
-var indece;
+/*  var indece;
 
 // Estrutura de repetição FOR
 
@@ -13,4 +13,16 @@ indece=1;
 while (indece <= 10){
     document.write('Hello World' +indece);
     indece++;
+}
+
+Estrutura de repetição 
+DO-WHILEindece= 1;
+do{ document.write('<br> Hello Word: '=indece);
+indece;} while(indece <= 10); */
+
+var alunos = ["Marcelo", "João", "Vinicius", "Redivo", "Laiz"];
+
+var i;
+for(i=0; i <= 3; i++){
+    document.write("Aluno "+i+": "+alunos[i]);
 }
