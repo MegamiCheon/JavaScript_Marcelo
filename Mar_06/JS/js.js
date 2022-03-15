883e2ce850c918c5b6d4ea5084f048e4d36ec4c4
LAIZ BORDINHAO DETROS <laiz.detros@alunos.sc.senac.br>
2022-03-15 14:18:52 -0300

    abcde

---
+++ Mar_06/JS/js.js
@@ -0,0 +1,16 @@
+var indece;
+
+// Estrutura de repetição FOR
+
+for (indeci=0; indece = 10; indece++){
+    document.write('<br>ola mundo' +indece)
+}
+
+// Estrutura de repetição WHILE
+
+indece=1;
+
+while (indece <= 10){
+    document.write('ola mundo' +indece);
+    indece++;
+}
